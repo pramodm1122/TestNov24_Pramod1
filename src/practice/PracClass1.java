2534f75adf994e00e859b578a5937312295b1b18
Pramod <pramodmane1122@gmail.com>
2024-12-14 12:11:05 +0530

    Branch A added

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -2,6 +2,6 @@ package practice;
 
 public class PracClass1 {
 	public static void main(String[] args) {
-		System.out.println("Hello_class1, Branch P changed.");
+		System.out.println("Hello_class1, Branch P changed., Second Scenario");
 	}
 }
