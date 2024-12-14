2d63edb25731a005a17273cd0691012c7819e5b7
Pramod <pramodmane1122@gmail.com>
2024-12-14 12:27:05 +0530

    Changes added in Branch B

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -2,6 +2,6 @@ package practice;
 
 public class PracClass1 {
 	public static void main(String[] args) {
-		System.out.println("Hello_class");
+		System.out.println("Hello_class, Added in Branch B");
 	}
 }
