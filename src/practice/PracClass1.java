1d81ed52f4563b3a32f10276983f8b8903782a50
Pramod <pramodmane1122@gmail.com>
2024-12-14 12:07:14 +0530

    P

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -3,10 +3,5 @@ package practice;
 public class PracClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1, Branch P changed.");
-<<<<<<< HEAD
-		System.out.println("Changes adding in P");
-=======
-		System.out.println("Changes added in M");
->>>>>>> f00978279d906559eb764001c5f5bdcd613ad6ac
 	}
 }
